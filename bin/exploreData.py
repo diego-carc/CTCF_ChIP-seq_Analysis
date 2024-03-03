6fc33693aa3592a7761482e5f9b7a26769c02cc4
Diego Carmona Campos <diegocar@lcg.unam.mx>
2024-03-03 12:00:31 -0600

    Tab delimited output

--- bin/exploreData.py
+++ bin/exploreData.py
@@ -1,4 +1,5 @@
 """
+Compute basic statistics about peaks in a bed file
 """
 
 # Import modules
@@ -12,8 +13,8 @@ args = parser.parse_args()
 
 # Explore Data
 data = pd.read_csv(args.filePath, sep='\t', header=None)
-print(f"Basic information from: {args.filePath}")
-print(f"Number of peaks: {data.shape[0]}")
-print(f"Sum of peak lengths: {sum(lenghts := data[2] - data[1])}") # Columns 1 and 2 correspond to sequence start and end
-print(f"Peak length statistics:\n{lenghts.describe()}")
-
+print(f"Basic information from:\t{args.filePath}")
+print(f"Number of peaks:\t{data.shape[0]}")
+print(f"Sum of peak lengths:\t{sum(lenghts := data[2] - data[1])}") # Columns 1 and 2 correspond to sequence start and end
+for key,value in lenghts.describe().to_dict().items():
+    print(f"{key}:\t{value}")
