3a28ce31ebc6726ff3061d366f81263d07864522
Diego Carmona Campos <diegocar@lcg.unam.mx>
2024-03-03 12:44:57 -0600

    Added Hocomoco motif to JASPAR reference motifs

--- data/CTCF_reference-motifs.tf
+++ data/CTCF_reference-motifs.tf
@@ -76,3 +76,39 @@ CC uniprot_ids:P49711
 CC data_type:ChIP-seq
 XX
 //
+AC  CTCF_HUMAN.H11MO.0.A
+XX
+ID  CTCF_HUMAN.H11MO.0.A
+XX
+DE  ygrCCAscAGrkGGCrsyv
+P0           a         c         g         t
+1         47.0     180.0      63.0     210.0
+2         76.0      84.0     261.0      79.0
+3        129.0      42.0     277.0      52.0
+4         34.0     428.0      18.0      20.0
+5          1.0     498.0       1.0       0.0
+6        350.0       3.0     121.0      26.0
+7          8.0     326.0     161.0       5.0
+8         49.0     297.0      37.0     117.0
+9        453.0       9.0      27.0      11.0
+10         1.0       1.0     496.0       2.0
+11       193.0       0.0     306.0       1.0
+12        13.0      13.0     294.0     180.0
+13         6.0       1.0     492.0       1.0
+14        25.0      13.0     430.0      32.0
+15        34.0     438.0       6.0      22.0
+16       198.0       7.0     292.0       3.0
+17        38.0     281.0     163.0      18.0
+18        48.0     211.0      58.0     183.0
+19       186.0     134.0     158.0      22.0
+XX
+CC  program: clusterbuster
+CC  matrix.nb: 1
+CC  consensus.strict: tggCCAccAGggGGCgcca
+CC  consensus.strict.rc: TGGCGCCCCCTGGTGGCCA
+CC  consensus.IUPAC: ygrCCAscAGrkGGCrsyv
+CC  consensus.IUPAC.rc: BRSYGCCMYCTGSTGGYCR
+CC  consensus.regexp: [ct]g[ag]CCA[cg]cAG[ag][gt]GGC[ag][cg][ct][acg]
+CC  consensus.regexp.rc: [CGT][AG][CG][CT]GCC[AC][CT]CTG[CG]TGG[CT]C[AG]
+XX
+//
